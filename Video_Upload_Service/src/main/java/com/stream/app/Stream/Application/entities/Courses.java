3957ddcd5069273a5e8d44b7055adf72536a4cb4
Anmol Kumar <anmolkumar8862@gmail.com>
2025-06-16 00:16:25 +0530

    Refractored the project towards MicroServices

---
+++ Video_Upload_Service/src/main/java/com/stream/app/Stream/Application/entities/Courses.java
@@ -0,0 +1,25 @@
+package com.stream.app.Stream.Application.entities;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Courses {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long CourseId;
+
+    private String name;
+
+
+//    @OneToMany(mappedBy = "Courses")
+//    private List<Video> videoList = new ArrayList<>();
+
+}
---
+++ Video_Upload_Service/src/main/java/com/stream/app/Stream/Application/repositories/videoRepo.java
@@ -0,0 +1,13 @@
+package com.stream.app.Stream.Application.repositories;
+
+import com.stream.app.Stream.Application.entities.Video;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface videoRepo extends JpaRepository<Video, Long> {
+
+    Optional<Video> findByTitle(String s);
+}
